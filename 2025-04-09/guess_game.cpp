7812fddd62c82609da5f72ba14df2493ebaecb7b
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-09 07:56:21 -0500

    se añade la condicion logica para saber si el numero coincid con el esperado o no

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -5,6 +5,11 @@ int main(void) {
     int guessed_number;
     std::cout << "adivina un numero entre 1 y 100:\n";
     std::cin >> guessed_number;
+    if (guessed_number == NUM) {
+        std::cout << "GANASTE!!!!!!!!!!!!!\n";
+    } else {
+        std::cout << "no ganaste, intentalo de nuevo\n";
+    }
 
     return 0;
 }
