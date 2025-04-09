e97a4690ff0aed84785c14d8c73440bb330940b7
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-09 07:32:24 -0500

    se añadio juego de adivinar clase 3

---
+++ 2025-04-09/guess_game.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guessed_number;
+    std::cout << "adivina un numero entre 1 y 100:\n";
+    std::cin >> guessed_number;
+
+    return 0;
+}
