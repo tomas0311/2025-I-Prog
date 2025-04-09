fd3e3f09794537ca3cfddd7ba174250604993ea1
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-09 08:54:50 -0500

    se añadio un bucle while en vez de for

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
+    #include <iostream>
 
-    int main(void) {
-        const int NUM = 10;
-        int guessed_number;
-        
-        for ( ; ; ){
-        std::cout << "adivina un numero entre 1 y 100:\n";
-        std::cin >> guessed_number;
-        if (guessed_number == NUM) {
-            std::cout << "GANASTE!!!!!!!!!!!!!\n";
-            break; 
-        } else if (guessed_number > NUM) {
-            std::cout << "Te pasaste.\n";
-        }
-            else {// es menor
-            std::cout << "Te falto.\n";
-        }
-        }
+        int main(void) {
+            const int NUM = 10;
+            int guessed_number = NUM + 1;
+            
+            while (guessed_number != NUM){
+            std::cout << "adivina un numero entre 1 y 100:\n";
+            std::cin >> guessed_number;
+            if (guessed_number == NUM) {
+                std::cout << "GANASTE!!!!!!!!!!!!!\n"; 
+            } else if (guessed_number > NUM) {
+                std::cout << "Te pasaste.\n";
+            }
+                else {// es menor
+                std::cout << "Te falto.\n";
+            }
+            }
 
-        return 0;
-}
+            return 0;
+    }
