778dfec19f88a00b702c7fa11f3486430010a6ed
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-09 08:32:34 -0500

    se añadio un loop for con un break para no repetir infinitamente

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
-int main(void) {
-    const int NUM = 10;
-    int guessed_number;
-    std::cout << "adivina un numero entre 1 y 100:\n";
-    std::cin >> guessed_number;
-    if (guessed_number == NUM) {
-        std::cout << "GANASTE!!!!!!!!!!!!!\n";
-    } else if (guessed_number > NUM) {
-        std::cout << "Te pasaste.\n";
-    }
-        else {// es menor
-         std::cout << "Te falto.\n";
-    }
+    int main(void) {
+        const int NUM = 10;
+        int guessed_number;
+        
+        for ( ; ; ){
+        std::cout << "adivina un numero entre 1 y 100:\n";
+        std::cin >> guessed_number;
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!!!!!!!!!!!\n";
+            break; 
+        } else if (guessed_number > NUM) {
+            std::cout << "Te pasaste.\n";
+        }
+            else {// es menor
+            std::cout << "Te falto.\n";
+        }
+        }
 
-    return 0;
+        return 0;
 }
