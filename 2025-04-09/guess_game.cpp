bd8aba9f09344ea9038056ef979ea058027722e3
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-11 08:09:14 -0500

    se añadio un condicional del rango para el numero, si se sale del mismo el programa termina

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -1,21 +1,30 @@
-    #include <iostream>
+        #include <iostream>
 
-        int main(void) {
-            const int NUM = 10;
-            int guessed_number = NUM + 1;
-            
-            while (guessed_number != NUM){
-            std::cout << "adivina un numero entre 1 y 100:\n";
-            std::cin >> guessed_number;
-            if (guessed_number == NUM) {
-                std::cout << "GANASTE!!!!!!!!!!!!!\n"; 
-            } else if (guessed_number > NUM) {
-                std::cout << "Te pasaste.\n";
-            }
-                else {// es menor
-                std::cout << "Te falto.\n";
-            }
-            }
+            int main(void) {
+                const int NUM = 10;
+                int guessed_number = NUM + 1;
+                
+                while (guessed_number != NUM){
+                std::cout << "adivina un numero entre 1 y 100:\n";
+                std::cin >> guessed_number;
+                std::cout << "escribiste " << guessed_number << "\n";
+                if (guessed_number > 100) {
+                    std::cout << "DEBE SER UN NUMERO MENOR O IGUAL A 100!!!!" << "\n";
+                    break ;
+                }
+                if (guessed_number < 1) {
+                    std::cout << "DEBE SER UN NUMERO MAYOR O IGUAL A 1!!!!" << "\n";
+                    break ;
+                }
+                if (guessed_number == NUM) {
+                    std::cout << "GANASTE!!!!!!!!!!!!!\n"; 
+                } else if (guessed_number > NUM) {
+                    std::cout << "Te pasaste.\n";
+                }
+                    else {// es menor
+                    std::cout << "Te falto.\n";
+                }
+                }
 
-            return 0;
-    }
+                return 0;
+        }
