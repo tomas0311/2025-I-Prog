0dc7b55bb7ac1bfe3f3b759e92234a0656e2490f
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-11 08:35:24 -0500

    se implemento variables a los rangos y se movio todo a una funcion play

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -1,19 +1,31 @@
             #include <iostream>
+            //declaracion de l funcion play
+            void play(void); 
 
                 int main(void) {
-                    const int NUM = 10;
+                    play();
+                    
+
+                    return 0;
+            }
+//implementacion
+void play(void)
+{
+    const int NUM = 10;
                     int guessed_number = NUM + 1;
+                    const int MIN = 1;
+                    const int MAX = 100;
                     
                     while (guessed_number != NUM){
-                    std::cout << "adivina un numero entre 1 y 100:\n";
+                    std::cout << "adivina un numero entre " << MIN << " y " << MAX << ":\n";
                     std::cin >> guessed_number;
                     std::cout << "escribiste " << guessed_number << "\n";
-                    if (guessed_number > 100) {
-                        std::cout << "DEBE SER UN NUMERO MENOR O IGUAL A 100!!!!" << "\n";
+                    if (guessed_number > MAX) {
+                        std::cout << "DEBE SER UN NUMERO MENOR O IGUAL A " << MAX << "!!!!" << "\n";
                         continue ;
                     }
-                    if (guessed_number < 1) {
-                        std::cout << "DEBE SER UN NUMERO MAYOR O IGUAL A 1!!!!" << "\n";
+                    if (guessed_number < MIN) {
+                        std::cout << "DEBE SER UN NUMERO MAYOR O IGUAL A " << MIN << "!!!!" << "\n";
                         continue ;
                     }
                     if (guessed_number == NUM) {
@@ -25,6 +37,4 @@
                         std::cout << "Te falto.\n";
                     }
                     }
-
-                    return 0;
-            }
+}
