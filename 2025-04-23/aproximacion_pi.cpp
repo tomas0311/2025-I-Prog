1c24b6e1ec4a106166167ec01dddccb38931da43
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-04-22 18:28:33 -0500

    se arreglaron detalles de interpretacion y estilo

--- 2025-04-23/aproximacion_pi.cpp
+++ 2025-04-23/aproximacion_pi.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip> 
 double pi_aprox (int iteraciones) ; 
 int main(void) {
     std::cout.precision(16);
     std::cout.setf(std::ios::scientific);
-    for (int n = 0 ; n <= 20 ; n++) {
+    std::cout << std::setw(4) << "n" << "  |  " << std::setw(25) << "Pi calculado" <<  "  |  " << std::setw(20) << "Error" << "\n";
+    for (int n = 1 ; n <= 20 ; n++) {
         double pi_error = std::abs(1 - pi_aprox(n)/M_PI);
 
-        std::cout <<  n <<  "  |  " << pi_aprox(n) << "  |  " << pi_error << "\n";
+        std::cout << std::setw(4) << n << "  |  " << std::setw(25) << pi_aprox(n) <<  "  |  " << std::setw(20) << pi_error << "\n";
         
     }
 
@@ -16,7 +18,7 @@ int main(void) {
 double pi_aprox (int iteraciones) {
     double n_termino = 0 ;
     
-    for ( int k = 0 ; k < iteraciones ; k++){
+    for ( int k = 0 ; k <= iteraciones ; k++){
           double nf_termino = (1/(std::pow (16,k))) * (4.0/(8*k + 1) - 2.0/(8*k + 4) - 1.0/(8*k + 5) - 1.0/(8*k + 6)) ;
          n_termino = n_termino + nf_termino ;
          
