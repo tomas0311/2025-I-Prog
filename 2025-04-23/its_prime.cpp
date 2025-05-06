79162a7839172cf2ae689f378f7b8850eb5bfd55
Tomás Jaramillo <tjaramilloh@unal.edu.co>
2025-05-06 17:53:51 -0500

    se generaron cambios en la funcion

--- 2025-04-23/its_prime.cpp
+++ 2025-04-23/its_prime.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <cmath>
-bool its_prime (long n);
+bool isprime (long n);
 int main(void) {
-    std::cout << 2 << ":" << its_prime(2) << "\n";
-    std::cout << 10000 << ":" << its_prime(10000) << "\n";
-    std::cout << 10000457 << ":" << its_prime(10000457) << "\n";
+    std::cout << 2 << ":" << isprime(2) << "\n";
+    std::cout << 10000 << ":" << isprime(10000) << "\n";
+    std::cout << 10000457 << ":" << isprime(10000457) << "\n";
     return 0;
 }
-bool its_prime (long n) {
-    bool flag = true ;
-    if (n <= 1){
-        std::cerr << "n esta fuera de rango";    
-        return false ;
+bool isprime(long n) {
+    if (n <= 1) {
+        std::cerr << "Out of range number: " << n << "\n";
+        return false;
     }
-    if ( n == 2){
-        return true ;
+    bool flag = true; 
+
+    if (n == 2) {
+        return true;
     }
-    for ( long i = 2 ; i <= std::sqrt(n) ; i++){
-        if ( n % i == 0){
-            flag = false ;
-            break;
+    for (long ii = 2; ii <= std::sqrt(n) + 1; ii++) {
+        if (n%ii == 0) {
+            flag = false;
+            break; 
         }
     }
-    return flag;
 
+    return flag;
 }
+
